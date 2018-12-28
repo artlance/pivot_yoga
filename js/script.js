32adf9e5a19656653fe587516b3c85ceac4bee4f
artlance <artlance.ru@gmail.com>
2018-12-28 11:09:30 +0200

    up

--- js/script.js
+++ js/script.js
@@ -376,6 +376,75 @@ $(document).ready(function() {
       ]
     });
 
+    //------------------------------------------------------------------------//
+
+    const countdown = function(_config) {
+      const tarDate = $(_config.target).data('date').split('-');
+      const day = parseInt(tarDate[0]);
+      const month = parseInt(tarDate[1]);
+      const year = parseInt(tarDate[2]);
+      let tarTime = $(_config.target).data('time');
+      let tarhour, tarmin;
+
+      if (tarTime != null) {
+        tarTime = tarTime.split(':');
+        tarhour = parseInt(tarTime[0]);
+        tarmin = parseInt(tarTime[1]);
+      }
+
+      let months = [31, new Date().getFullYear() % 4 == 0 ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      let dateNow = new Date();
+      let dayNow = dateNow.getDate();
+      let monthNow = dateNow.getMonth() + 1;
+      let yearNow = dateNow.getFullYear();
+      let hourNow = dateNow.getHours();
+      let minNow = dateNow.getMinutes();
+      let count_day = 0, count_hour = 0, count_min = 0;
+      let count_day_isSet = false;
+      let isOver = false;
+      const countDownDate = new Date(year, month-1, day, tarhour, tarmin, 0, 0).getTime();
+
+      $(_config.target+' .day .word').html(_config.dayWord);
+      $(_config.target+' .hour .word').html(_config.hourWord);
+      $(_config.target+' .min .word').html(_config.minWord);
+      $(_config.target+' .sec .word').html(_config.secWord);
+
+      const updateTime = () => {
+
+        const now = new Date().getTime();
+        const distance = countDownDate - now;
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        requestAnimationFrame(updateTime);
+
+        $(_config.target+' .day .num').html(addZero(days));
+        $(_config.target+' .hour .num').html(addZero(hours));
+        $(_config.target+' .min .num').html(addZero(minutes));
+        $(_config.target+' .sec .num').html(addZero(seconds));
+
+        if (distance < 0) {
+          $(".message-global").remove();
+        }
+      }
+
+      updateTime();
+    }
+
+    const addZero = (x) => (x < 10 && x >= 0) ? "0"+x : x;
+
+    const efcc_countdown = new countdown({
+      target: '.countdown',
+      dayWord: ' days',
+      hourWord: ' hours',
+      minWord: ' mins',
+      secWord: ' secs'
+    });
+
+
+    //------------------------------------------------------------------------//
 
 }); //document ready
 
